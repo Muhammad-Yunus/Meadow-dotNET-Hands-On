f74cdd35284a51204a313973ad5a1dd0753bcd7a
Muhammad Yunus <34908065+Muhammad-Yunus@users.noreply.github.com>
2021-11-27 22:43:59 +0700

    update

---
+++ 4_AnalogRead_LM35/4_AnalogRead_LM35/MeadowApp.cs
@@ -0,0 +1,34 @@
+﻿using Meadow;
+using Meadow.Devices;
+using Meadow.Units;
+using Meadow.Foundation.Sensors.Temperature;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace _4_AnalogRead_LM35
+{
+    public class MeadowApp : App<F7Micro, MeadowApp>
+    {
+        AnalogTemperature analogTemperature;
+
+        public MeadowApp()
+        {
+            analogTemperature = new AnalogTemperature(
+                Device, Device.Pins.A00, AnalogTemperature.KnownSensorType.LM35
+               );
+            while (true)
+            {
+                ReadTemp().Wait();
+                Thread.Sleep(3000);
+            }
+        }
+
+        async Task ReadTemp()
+        {
+            Temperature temperature = await analogTemperature.Read();
+            Console.WriteLine($"Temperature : {temperature.Celsius:N2}C");
+        }
+
+    }
+}
