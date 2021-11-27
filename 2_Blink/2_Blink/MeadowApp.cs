4b47a055ba338f0dcbcbcb2c1bd03e89c6763fa1
Muhammad Yunus <34908065+Muhammad-Yunus@users.noreply.github.com>
2021-11-27 21:41:30 +0700

    update

---
+++ 2_Blink/2_Blink/MeadowApp.cs
@@ -0,0 +1,28 @@
+﻿using Meadow;
+using Meadow.Devices;
+using Meadow.Foundation;
+using Meadow.Foundation.Leds;
+using System;
+using System.Threading;
+
+namespace _2_Blink
+{
+    public class MeadowApp : App<F7Micro, MeadowApp>
+    {
+
+        public MeadowApp()
+        {
+            var pwmLed = new PwmLed(
+                    Device,
+                    Device.Pins.OnboardLedGreen,
+                    TypicalForwardVoltage.Green);
+
+            // blink the LED
+            pwmLed.StartBlink(
+                onDuration : 1000, 
+                offDuration : 1000
+                );
+        }
+
+    }
+}
---
+++ 3_Pulse/3_Pulse/MeadowApp.cs
@@ -0,0 +1,20 @@
+﻿using Meadow;
+using Meadow.Devices;
+using Meadow.Foundation;
+using Meadow.Foundation.Leds;
+using System;
+using System.Threading;
+
+namespace _3_Pulse
+{
+    public class MeadowApp : App<F7Micro, MeadowApp>
+    {
+        public MeadowApp()
+        {
+            var pwmLed = new PwmLed(Device, Device.Pins.D13, TypicalForwardVoltage.Blue);
+
+            // pulse tehe LED
+            pwmLed.StartPulse(pulseDuration: 1000, lowBrightness:0);
+        }
+    }
+}
