fa111ed4aaf1a1e99a604d4e743083ddf49bc8bf
Muhammad Yunus <34908065+Muhammad-Yunus@users.noreply.github.com>
2021-11-27 22:47:51 +0700

    uodate

---
+++ 5_AnalogRead_LM35_Automatic_Pooling/5_AnalogRead_LM35_Automatic_Pooling/MeadowApp.cs
@@ -0,0 +1,18 @@
+﻿using Meadow;
+using Meadow.Devices;
+using Meadow.Foundation;
+using Meadow.Foundation.Leds;
+using System;
+using System.Threading;
+
+namespace _5_AnalogRead_LM35_Automatic_Pooling
+{
+    public class MeadowApp : App<F7Micro, MeadowApp>
+    {
+
+        public MeadowApp()
+        {
+
+        }
+    }
+}
