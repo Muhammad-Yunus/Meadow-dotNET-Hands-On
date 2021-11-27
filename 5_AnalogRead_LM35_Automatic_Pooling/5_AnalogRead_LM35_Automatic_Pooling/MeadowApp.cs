2640132361cb88371317aa5562408763fae0d523
Muhammad Yunus <34908065+Muhammad-Yunus@users.noreply.github.com>
2021-11-28 06:57:40 +0700

    update

--- 5_AnalogRead_LM35_Automatic_Pooling/5_AnalogRead_LM35_Automatic_Pooling/MeadowApp.cs
+++ 5_AnalogRead_LM35_Automatic_Pooling/5_AnalogRead_LM35_Automatic_Pooling/MeadowApp.cs
@@ -1,18 +1,31 @@
 ﻿using Meadow;
 using Meadow.Devices;
-using Meadow.Foundation;
-using Meadow.Foundation.Leds;
+using Meadow.Units;
+using Meadow.Foundation.Sensors.Temperature;
 using System;
 using System.Threading;
+using System.Threading.Tasks;
 
 namespace _5_AnalogRead_LM35_Automatic_Pooling
 {
     public class MeadowApp : App<F7Micro, MeadowApp>
     {
 
+        AnalogTemperature analogTemperature;
+
         public MeadowApp()
         {
+            analogTemperature = new AnalogTemperature(
+                Device, Device.Pins.A00, AnalogTemperature.KnownSensorType.LM35
+               );
 
+            analogTemperature.TemperatureUpdated += (s, result) =>
+            {
+                Console.WriteLine($"Temp Changed, temp: {result.New.Celsius:N2}C, old: {result.Old?.Celsius:N2}C");
+            };
+
+            analogTemperature.StartUpdating(TimeSpan.FromSeconds(1));
         }
+
     }
 }
