d519018f94c38269792b4cbd4bea04feb9759f6a
Muhammad Yunus <34908065+Muhammad-Yunus@users.noreply.github.com>
2021-11-28 13:47:22 +0700

    update

---
+++ 18_Fading_LED_Foundation_PWM_using_Button_Interrupt/18_Fading_LED_Foundation_PWM_using_Button_Interrupt/MeadowApp.cs
@@ -0,0 +1,49 @@
+﻿using Meadow;
+using Meadow.Devices;
+using Meadow.Hardware;
+using Meadow.Foundation;
+using Meadow.Foundation.Leds;
+using System;
+using System.Threading;
+
+namespace _18_Fading_LED_Foundation_PWM_using_Button_Interrupt
+{
+    public class MeadowApp : App<F7Micro, MeadowApp>
+    {
+        PwmLed pwm13;
+        IDigitalInputPort digitalIn12;
+
+        public MeadowApp()
+        {
+            Initialize();
+            loop();
+
+        }
+
+        void Initialize()
+        {
+            Console.WriteLine("Initialize hardware...");
+
+            pwm13 = new PwmLed(Device, Device.Pins.D13, TypicalForwardVoltage.Green);
+
+            digitalIn12 = Device.CreateDigitalInputPort(Device.Pins.D12, InterruptMode.EdgeRising, ResistorMode.InternalPullDown, debounceDuration: 500);
+
+            digitalIn12.Changed += (s, result) =>
+            {
+                Console.WriteLine($"Interrupt occured, state : {result.New.State}, start fading LED!");
+                pwm13.StartPulse(pulseDuration: 1000);
+                Thread.Sleep(3000);
+                pwm13.Stop();
+            };
+        }
+
+        void loop()
+        {
+            while (true)
+            {
+                Console.WriteLine($"Input state : {digitalIn12.State}");
+                Thread.Sleep(1000);
+            }
+        }
+    }
+}
---
+++ 18_Fading_LED_Foundation_PWM_using_Button_Interrupt/18_Fading_LED_Foundation_PWM_using_Button_Interrupt/Program.cs
@@ -0,0 +1,19 @@
+﻿using Meadow;
+using System.Threading;
+
+namespace _18_Fading_LED_Foundation_PWM_using_Button_Interrupt
+{
+    class Program
+    {
+        static IApp app;
+        public static void Main(string[] args)
+        {
+            if (args.Length > 0 && args[0] == "--exitOnDebug") return;
+
+            // instantiate and run new meadow app
+            app = new MeadowApp();
+
+            Thread.Sleep(Timeout.Infinite);
+        }
+    }
+}
