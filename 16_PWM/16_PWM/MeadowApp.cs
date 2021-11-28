5219e22ecd39a56376183d369906b44310238ea0
Muhammad Yunus <34908065+Muhammad-Yunus@users.noreply.github.com>
2021-11-28 13:21:58 +0700

    update

---
+++ 16_PWM/16_PWM/MeadowApp.cs
@@ -0,0 +1,41 @@
+﻿using Meadow;
+using Meadow.Devices;
+using Meadow.Hardware;
+using System;
+using System.Threading;
+
+namespace _16_PWM
+{
+    public class MeadowApp : App<F7Micro, MeadowApp>
+    {
+        IPwmPort pwm13;
+        public MeadowApp()
+        {
+            Initialize();
+            loop();
+        }
+
+        void Initialize()
+        {
+            Console.WriteLine("Initialize hardware...");
+
+            // hardware PWM
+            pwm13 = Device.CreatePwmPort(Device.Pins.D13, frequency: 100, dutyCycle: 0.5f, inverted:false);
+
+            pwm13.Start();
+        }
+
+        void loop()
+        {
+            while (true)
+            {
+                for(var i=0; i < 100; i++)
+                {
+                    pwm13.DutyCycle = (float)(i/100.0);
+                    Console.WriteLine($"Duty Cycle : {pwm13.DutyCycle}");
+                    Thread.Sleep(10);
+                }
+            }
+        }
+    }
+}
---
+++ 17_Fading_LED_Using_Button_Interupt/17_Fading_LED_Using_Button_Interupt/MeadowApp.cs
@@ -0,0 +1,63 @@
+﻿using Meadow;
+using Meadow.Devices;
+using Meadow.Hardware;
+using System;
+using System.Threading;
+
+namespace _17_Fading_LED_Using_Button_Interupt
+{
+    public class MeadowApp : App<F7Micro, MeadowApp>
+    {
+        IPwmPort pwm13;
+        IDigitalInputPort digitalIn12;
+
+        public MeadowApp()
+        {
+            Initialize();
+            loop();
+        }
+
+        void Initialize()
+        {
+            Console.WriteLine("Initialize hardware...");
+
+            pwm13 = Device.CreatePwmPort(Device.Pins.D13, frequency: 100, dutyCycle: 0.0f);
+            
+            pwm13.Start();
+
+            digitalIn12 = Device.CreateDigitalInputPort(Device.Pins.D12, InterruptMode.EdgeRising, ResistorMode.InternalPullDown, debounceDuration: 500);
+
+            digitalIn12.Changed += (s, result) =>
+            {
+                if (result.New.State == true)
+                {
+                    for (var i = 0; i < 3; i++)
+                    {
+                        for (var j = 0; j < 100; j++)
+                        {
+                            pwm13.DutyCycle = (float)(j / 100.0);
+                            Thread.Sleep(10);
+                        }
+                        for (var j = 100; j >= 0; j--)
+                        {
+                            pwm13.DutyCycle = (float)(j / 100.0);
+                            Thread.Sleep(10);
+                        }
+                    }
+                }
+            };
+
+        }
+
+        void loop()
+        {
+            while (true)
+            {
+                Console.WriteLine($"Current Duty Cycle : {pwm13.DutyCycle}");
+                Thread.Sleep(1000);
+            }
+        }
+
+
+    }
+}
